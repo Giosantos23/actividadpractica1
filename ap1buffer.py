4c5b2f37667a1c88bcd917d8bf241719affcbdc6
Giovanni Santos <giovannisantos1890@gmail.com>
2025-01-25 19:56:21 -0600

    funcionalidad buffer

--- ap1buffer.py
+++ ap1buffer.py
@@ -5,3 +5,35 @@ def cargar_buffer(entrada, inicio, tamano_buffer):
     return buffer
 
 
+def procesar_buffer(entrada, tamano_buffer):
+    inicio = 0
+    avance = 0
+    buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+    lexema = ""
+    
+    while True:
+        char = buffer[avance]
+        
+        if char == "eof":  
+            if lexema: 
+                print(f"Lexema procesado: {lexema}")
+            break
+        
+        if char.isspace():  
+            if lexema: 
+                print(f"Lexema procesado: {lexema}")
+                lexema = ""
+        else:
+            lexema += char  
+        
+        avance += 1
+        
+        if avance >= len(buffer):
+            inicio += tamano_buffer
+            buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+            avance = 0
+
+
+entrada = list("Esto es un ejemplo eof")
+tamano_buffer = 10
+procesar_buffer(entrada, tamano_buffer)
